8013dcd20296f4a906e52cd703073577d72567bc
Christopher Olsen <olsen.criss@gmail.com>
2022-04-28 10:49:37 +0200

    Add classes for clubs

---
+++ pages/classes.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    <TheNavBar />
+
+<main>
+    <h1>Join a Scratch Class!</h1>
+    <div>Remember that you must not use your real name or anything that would help to know who you are in real life. Protect your identity!</div>
+<br />
+    <h2>
+        Sedgefield Academy of Excellence join link: <a href="http://scratch.mit.edu/signup/6yj4n2h5k">Join Class</a>
+    </h2>
+<br />
+<br />
+    <h2>
+        Friday Coding and Robotics Clubs: <a href="http://scratch.mit.edu/signup/t2jjtcfkr">Join Class</a>
+    </h2>
+</main>
+</div>
+</template>
+
+<style scoped>
+main {
+    max-width: 600px;
+    margin: 0 auto;
+}
+</style>
