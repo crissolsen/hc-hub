3cc9b780cef4cda72ee1cd9ba3f90763a48aad50
Christopher Olsen <olsen.criss@gmail.com>
2022-06-14 14:42:41 +0200

    Try Heroku out often to see if it powers down...

--- netlify/functions/fetch-courses/fetch-courses.js
+++ netlify/functions/fetch-courses/fetch-courses.js
@@ -5,12 +5,20 @@ const handler = async function (event, context, callback) {
   try {
     const type = event.queryStringParameters.classes
 
-    const url = "http://nocotrial.crissolsen.repl.co/api/v1/db/data/noco/HomeCode"
+    // Replit url
+    // const url = "http://nocotrial.crissolsen.repl.co/api/v1/db/data/noco/HomeCode"
 
+    // Heroku implementation
+    const url = "http://homecodeza.herokuapp.com/api/v1/db/data/noco/p_4kntampuate9aa"
+
+    // Heroku Key
+    const xcKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRldkBob21lY29kZS5jby56YSIsImZpcnN0bmFtZSI6bnVsbCwibGFzdG5hbWUiOm51bGwsImlkIjoidXNfdWg3cnlkaDZudnd3bzUiLCJyb2xlcyI6InVzZXIsc3VwZXIiLCJpYXQiOjE2NTQ4NjAyMDh9.AHtktV-awku5SotTN0ONYgYtDr-vn_27DNxlk9csMyg"
+    // Replit key
+    // const xcKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRldkBob21lY29kZS5jby56YSIsImZpcnN0bmFtZSI6bnVsbCwibGFzdG5hbWUiOm51bGwsImlkIjoidXNfMm11OGN0cXlpMXZlajUiLCJyb2xlcyI6InVzZXIsc3VwZXIiLCJpYXQiOjE2NTUyMDQ4NzF9.rNpcxdGzO8Gp9X9lXYrKIAiyCIiHi1G2-yzmCY89cUU"
     let typeSearch = type == 'view' ? "/Courses/views/Courses" : "/Learners/views/Learners"
 
     const response = await fetch(`${url}${typeSearch}`, {
-      headers: { Accept: 'application/json', 'xc-auth': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRldkBob21lY29kZS5jby56YSIsImZpcnN0bmFtZSI6bnVsbCwibGFzdG5hbWUiOm51bGwsImlkIjoidXNfMm11OGN0cXlpMXZlajUiLCJyb2xlcyI6InVzZXIsc3VwZXIiLCJpYXQiOjE2NTUyMDQ4NzF9.rNpcxdGzO8Gp9X9lXYrKIAiyCIiHi1G2-yzmCY89cUU' },
+      headers: { Accept: 'application/json', 'xc-auth': `${ xcKey }` },
     })
     console.log(response)
     if (!response.ok) {
